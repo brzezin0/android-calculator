5ea34fead5b354c228432c32e20aaba12a57ac73
brzezin <236700@edu.p.lodz.pl>
2024-03-17 00:03:25 +0100

    added About Activity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculator.ui.theme.CalculatorTheme
+import androidx.activity.compose.setContent
+import android.view.View;
+import android.content.Intent;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val aboutButton = findViewById<Button>(R.id.aboutButton)
+
+        aboutButton.setOnClickListener{view->
+            val intent = Intent(this,AboutActivity::class.java);
+            startActivity(intent)
+        }
     }
 }
 
